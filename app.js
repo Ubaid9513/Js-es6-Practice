021f5b2487c0021f7829158094f23f2ce89f7978
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-07-10 23:56:51 +0500

    practice ternary operator

--- app.js
+++ app.js
@@ -55,10 +55,10 @@
 
 // ----------------------{TEMPLATE LITERALS}---------------------
 
-let firstName = "Ubaid";
-let lastName = "Aslam";
-console.log("My name is " + firstName + " " + lastName);
-console.log(`My name is ${firstName} ${lastName}`);
+// let firstName = "Ubaid";
+// let lastName = "Aslam";
+// console.log("My name is " + firstName + " " + lastName);
+// console.log(`My name is ${firstName} ${lastName}`);
 
 // ----------------------{SPREAD OPERATORS}---------------------
 
@@ -71,40 +71,46 @@ console.log(`My name is ${firstName} ${lastName}`);
 // let student2 = [...student1,"Uzaima","Atif"];
 // console.log(student2);
 // ---------------x--------------------
-let student1 = { name: "Ubaid" };
-let student2 = { ...student1, school: "Saylani" };
-console.log(student2);
+// let student1 = { name: "Ubaid" };
+// let student2 = { ...student1, school: "Saylani" };
+// console.log(student2);
 
 // ----------------------{OBJECT DESTRUCTURING}---------------------
-var student = {
-  name: "Ubaid",
-  roll: 215783,
-  sch: "Saylani",
-};
-console.log(student);
-// ---------------x--------------------
-var student = {
-  name: "Ubaid",
-  roll: 215783,
-  sch: "Saylani",
-};
-let { name, sch, roll } = student;
-console.log(name);
-console.log(sch);
-console.log(roll);
+// var student = {
+//   name: "Ubaid",
+//   roll: 215783,
+//   sch: "Saylani",
+// };
+// console.log(student);
+// // ---------------x--------------------
+// var student = {
+//   name: "Ubaid",
+//   roll: 215783,
+//   sch: "Saylani",
+// };
+// let { name, sch, roll } = student;
+// console.log(name);
+// console.log(sch);
+// console.log(roll);
 
 // ----------------------{TERNARY OPERATORS}---------------------
 // // ternary opr m (|| and &&) bhi lga sakhte hen
-var age = 20;
-if (age > 30) {
-  console.log("30 se bara hai");
-} else {
-  console.log("30 se chota hai");
-}
-// ---------------x--------------------
-var check = age > 30 ? "30 se bara hai" : "30 se chota hai";
-console.log(check);
-// ---------------x--------------------
-var bool = true;
-var foo = bool && "Ubaid";
-console.log(foo);
+// var age = 20;
+// if (age > 30) {
+//   console.log("30 se bara hai");
+// } else {
+//   console.log("30 se chota hai");
+// }
+// // ---------------x--------------------
+// var check = age > 30 ? "30 se bara hai" : "30 se chota hai";
+// console.log(check);
+// // ---------------x--------------------
+// var bool = true;
+// var foo = bool && "Ubaid";
+// console.log(foo);
+// // ---------------x--------------------
+// var check2 = age > 30 || age == 30 ? "30 se bara hai" : "30 se chota hai";
+// console.log(check2);
+// // ---------------x--------------------
+// var check3 = age > 40 ? "kafi bara hai" : age == 30 ? "30 ke brabar hai" : age > 30 ? "30 se bara hai" : "30 se chota hai" ;
+// console.log(check3);
